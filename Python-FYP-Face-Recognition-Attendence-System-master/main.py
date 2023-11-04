b400af3d513fb5cb5435b0f3dcf560f0a5c6f522
Tahmin-Rahman <rahmantahmin64@gmail.com>
2023-11-05 02:44:24 +0600

    first commit

---
+++ Python-FYP-Face-Recognition-Attendence-System-master/main.py
@@ -0,0 +1,176 @@
+from tkinter import*
+from tkinter import ttk
+from train import Train
+from PIL import Image,ImageTk
+from student import Student
+from train import Train
+from face_recognition import Face_Recognition
+from attendance import Attendance
+from developer import Developer
+import os
+from helpsupport import Helpsupport
+
+class Face_Recognition_System:
+    def __init__(self,root):
+        self.root=root
+        self.root.geometry("1366x768+0+0")
+        self.root.title("Face_Recogonition_System")
+
+# This part is image labels setting start 
+        # first header image  
+        img=Image.open("Python-FYP-Face-Recognition-Attendence-System-master/images/Download premium image of Beige abstract wallpaper background image  by Nunny about sand, sand texture, earth, watercolor beige, and earth tone 2620107.jpg")
+        img=img.resize((1366,130),Image.ANTIALIAS)
+        self.photoimg=ImageTk.PhotoImage(img)
+
+        # set image as lable
+        f_lb1 = Label(self.root,image=self.photoimg)
+        f_lb1.place(x=0,y=0,width=1366,height=130)
+
+        # backgorund image 
+        bg1=Image.open("Python-FYP-Face-Recognition-Attendence-System-master/images/Download premium image of Pink beige tile texture background illustration by Nunny about pink beige tile texture background illustration, beige distorted geometric square tile texture background, brown dist.jpg")
+        bg1=bg1.resize((1366,768),Image.ANTIALIAS)
+        self.photobg1=ImageTk.PhotoImage(bg1)
+
+        # set image as lable
+        bg_img = Label(self.root,image=self.photobg1)
+        bg_img.place(x=0,y=130,width=1366,height=768)
+
+
+        #title section
+        title_lb1 = Label(bg_img,text="TONN SOFTWARE",font=("verdana",30,"bold"),bg="white",fg="navyblue")
+        title_lb1.place(x=0,y=0,width=1366,height=45)
+
+        # Create buttons below the section 
+        # ------------------------------------------------------------------------------------------------------------------- 
+        # student button 1
+        std_img_btn=Image.open("Python-FYP-Face-Recognition-Attendence-System-master/images/Premium Vector _ Student boy character illustration_.jpg")
+        std_img_btn=std_img_btn.resize((180,180),Image.ANTIALIAS)
+        self.std_img1=ImageTk.PhotoImage(std_img_btn)
+
+        std_b1 = Button(bg_img,command=self.student_pannels,image=self.std_img1,cursor="hand2")
+        std_b1.place(x=250,y=100,width=180,height=180)
+
+        std_b1_1 = Button(bg_img,command=self.student_pannels,text="Student Pannel",cursor="hand2",font=("tahoma",15,"bold"),bg="white",fg="navyblue")
+        std_b1_1.place(x=250,y=280,width=180,height=45)
+
+        # Detect Face  button 2
+        det_img_btn=Image.open("Python-FYP-Face-Recognition-Attendence-System-master/images/cover tutorials & tips  - constellation on face.jpg")
+        det_img_btn=det_img_btn.resize((180,180),Image.ANTIALIAS)
+        self.det_img1=ImageTk.PhotoImage(det_img_btn)
+
+        det_b1 = Button(bg_img,command=self.face_rec,image=self.det_img1,cursor="hand2",)
+        det_b1.place(x=480,y=100,width=180,height=180)
+
+        det_b1_1 = Button(bg_img,command=self.face_rec,text="Face Detector",cursor="hand2",font=("tahoma",15,"bold"),bg="white",fg="navyblue")
+        det_b1_1.place(x=480,y=280,width=180,height=45)
+
+         # Attendance System  button 3
+        att_img_btn=Image.open("Python-FYP-Face-Recognition-Attendence-System-master/images/Attendance presence icon.jpg")
+        att_img_btn=att_img_btn.resize((180,180),Image.ANTIALIAS)
+        self.att_img1=ImageTk.PhotoImage(att_img_btn)
+
+        att_b1 = Button(bg_img,command=self.attendance_pannel,image=self.att_img1,cursor="hand2",)
+        att_b1.place(x=710,y=100,width=180,height=180)
+
+        att_b1_1 = Button(bg_img,command=self.attendance_pannel,text="Attendance",cursor="hand2",font=("tahoma",15,"bold"),bg="white",fg="navyblue")
+        att_b1_1.place(x=710,y=280,width=180,height=45)
+
+         # Help  Support  button 4
+        hlp_img_btn=Image.open("Python-FYP-Face-Recognition-Attendence-System-master/images/Premium Vector _ Collection of tattoos with abstract shapes.jpg")
+        hlp_img_btn=hlp_img_btn.resize((180,180),Image.ANTIALIAS)
+        self.hlp_img1=ImageTk.PhotoImage(hlp_img_btn)
+
+        hlp_b1 = Button(bg_img,command=self.helpSupport,image=self.hlp_img1,cursor="hand2",)
+        hlp_b1.place(x=940,y=100,width=180,height=180)
+
+        hlp_b1_1 = Button(bg_img,command=self.helpSupport,text="Help Support",cursor="hand2",font=("tahoma",15,"bold"),bg="white",fg="navyblue")
+        hlp_b1_1.place(x=940,y=280,width=180,height=45)
+
+        # Top 4 buttons end.......
+        # ---------------------------------------------------------------------------------------------------------------------------
+        # Start below buttons.........
+         # Train   button 5
+        tra_img_btn=Image.open("Python-FYP-Face-Recognition-Attendence-System-master/images/Facial Recognition and Fingerprint Scanning – Which is better_.jpg")
+        tra_img_btn=tra_img_btn.resize((180,180),Image.ANTIALIAS)
+        self.tra_img1=ImageTk.PhotoImage(tra_img_btn)
+
+        tra_b1 = Button(bg_img,command=self.train_pannels,image=self.tra_img1,cursor="hand2",)
+        tra_b1.place(x=250,y=330,width=180,height=180)
+
+        tra_b1_1 = Button(bg_img,command=self.train_pannels,text="Data Train",cursor="hand2",font=("tahoma",15,"bold"),bg="white",fg="navyblue")
+        tra_b1_1.place(x=250,y=510,width=180,height=45)
+
+        # Photo   button 6
+        pho_img_btn=Image.open("Python-FYP-Face-Recognition-Attendence-System-master/images/Facial Recognition_ All you Need to Know - Algorithm-X Lab.jpg")
+        pho_img_btn=pho_img_btn.resize((180,180),Image.ANTIALIAS)
+        self.pho_img1=ImageTk.PhotoImage(pho_img_btn)
+
+        pho_b1 = Button(bg_img,command=self.open_img,image=self.pho_img1,cursor="hand2",)
+        pho_b1.place(x=480,y=330,width=180,height=180)
+
+        pho_b1_1 = Button(bg_img,command=self.open_img,text="QR-Codes",cursor="hand2",font=("tahoma",15,"bold"),bg="white",fg="navyblue")
+        pho_b1_1.place(x=480,y=510,width=180,height=45)
+
+        # Developers   button 7
+        dev_img_btn=Image.open("Python-FYP-Face-Recognition-Attendence-System-master/images/Designers Challenge Themselves To Create A Typographic Logo Every Day For A Year, And They’re Pretty Cool.png")
+        dev_img_btn=dev_img_btn.resize((180,180),Image.ANTIALIAS)
+        self.dev_img1=ImageTk.PhotoImage(dev_img_btn)
+
+        dev_b1 = Button(bg_img,command=self.developr,image=self.dev_img1,cursor="hand2",)
+        dev_b1.place(x=710,y=330,width=180,height=180)
+
+        dev_b1_1 = Button(bg_img,command=self.developr,text="Developers",cursor="hand2",font=("tahoma",15,"bold"),bg="white",fg="navyblue")
+        dev_b1_1.place(x=710,y=510,width=180,height=45)
+
+        # exit   button 8
+        exi_img_btn=Image.open("Python-FYP-Face-Recognition-Attendence-System-master/images/Exit, vector icon.jpg")
+        exi_img_btn=exi_img_btn.resize((180,180),Image.ANTIALIAS)
+        self.exi_img1=ImageTk.PhotoImage(exi_img_btn)
+
+        exi_b1 = Button(bg_img,command=self.Close,image=self.exi_img1,cursor="hand2",)
+        exi_b1.place(x=940,y=330,width=180,height=180)
+
+        exi_b1_1 = Button(bg_img,command=self.Close,text="Exit",cursor="hand2",font=("tahoma",15,"bold"),bg="white",fg="navyblue")
+        exi_b1_1.place(x=940,y=510,width=180,height=45)
+
+# ==================Funtion for Open Images Folder==================
+    def open_img(self):
+        os.startfile("dataset")
+# ==================Functions Buttons=====================
+    def student_pannels(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Student(self.new_window)
+
+    def train_pannels(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Train(self.new_window)
+    
+    def face_rec(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Face_Recognition(self.new_window)
+    
+    def attendance_pannel(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Attendance(self.new_window)
+    
+    def developr(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Developer(self.new_window)
+    
+    def helpSupport(self):
+        self.new_window=Toplevel(self.root)
+        self.app=Helpsupport(self.new_window)
+
+    def Close(self):
+        root.destroy()
+    
+    
+
+
+
+
+
+if __name__ == "__main__":
+    root=Tk()
+    obj=Face_Recognition_System(root)
+    root.mainloop()
